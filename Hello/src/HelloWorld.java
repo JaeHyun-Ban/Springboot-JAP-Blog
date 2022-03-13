f8cd4a9bf03f8256363947f4bac5b695b04b63f8
JaeHyun-Ban <wogus0008@gmail.com>
2022-03-13 23:33:19 +0900

    03-13

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+
+//이클립스 = ctrl + F11
+//KeyMap = 인텔리J = shift + f10
+public class HelloWorld {
+	//스프링부트 연습을 위한 첫번째 테스트!!
+	public static void main(String[] args) {
+		//Ctrl + D = 복사
+		System.out.println("헬로우 월드");
+		System.out.println("헬로우 월드");
+	}
+	
+	
+}
